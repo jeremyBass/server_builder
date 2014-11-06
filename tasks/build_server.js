6c55cd5479c2e87b9bacda2f0bdb29c378dc69d3
Jeremy Bass <jeremybass@cableone.net>
2014-11-06 01:09:08 -0800

    start the  server builder

--- tasks/build_server.js
+++ tasks/build_server.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
-  grunt.registerTask('build_server', 'Building the server', function() {
-    grunt.log.writeln("Would start building the sever");
-  });
+	grunt.registerTask('build_server', 'Building the server', function() {
+		grunt.log.writeln("Would start building the sever");
+		function cmd_exec(cmd, args, cb_stdout, cb_end) {
+			var spawn = require('child_process').spawn,
+				child = spawn(cmd, args),
+				me = this;
+			me.exit = 0;  // Send a cb to set 1 when cmd exits
+			child.stdout.on('data', function (data) { cb_stdout(me, data) });
+			child.stdout.on('end', function () { cb_end(me) });
+		}
+		
+		var nunjucks = require('nunjucks');
+		var fs = require('fs');
+		var extend = require('extend');
+		var wrench = require('wrench'),
+			util = require('util');
+		
+		grunt.task.run(['build_salt']);
+		
+		var sourceDir = 'server/salt';
+		var targetDir = '/srv/salt';
+		wrench.copyDirSyncRecursive(sourceDir,targetDir);
+		grunt.log.writeln(sourceDir+" >> "+targetDir);
+	});
 };
