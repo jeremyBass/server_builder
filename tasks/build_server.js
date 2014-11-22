fb3fbc0ed1ed5c9eb99effce99ae14d574e6ea6d
Jeremy Bass <jeremybass@cableone.net>
2014-11-22 14:00:35 -0800

    try to abstract to root an error out

--- tasks/build_server.js
+++ tasks/build_server.js
@@ -9,23 +9,36 @@ module.exports = function(grunt) {
 			util = require('util'),
 			spawn = require('child_process').spawn;
 
-		
-		if (!fs.existsSync("/srv/salt")) {
-			fs.mkdir("/srv/salt", 0777, true, function (err) {
-				if (err) {
-					grunt.log.writeln("failed to make folder server");
-				} else {
-					grunt.log.writeln("made folder server");
+
+
+
+		function run_env(env_obj){
+			var current_env = env_obj[0];
+			env_obj.shift();
+			ls    = spawn('salt-call', ['--local','--log-level=info','--config-dir=/etc/salt','state.highstate','env='+current_env]);
+			var lastout;
+			ls.stdout.on('data', function (data) {
+				var out = data.toString().trim();
+				if( out!='\n' && out!=null && out!="" && lastout!=out){
+					lastout=out;
+					console.log(out);
+				}
+			});
+			ls.stderr.on('data', function (data) {
+			  console.log('stderr: ' + data);
+			});
+			ls.on('exit', function (code) {
+				console.log('child process exited with code ' + code);
+				grunt.log.writeln("<<<<<<<< finished sever "+current_env);
+
+				if(env_obj.length>0){
+					run_env(env_obj);
 				}
 			});
 		}
-		var sourceDir = 'tasks/jigs/salt';
-		var targetDir = '/srv/salt';
-		grunt.log.writeln("about to move "+sourceDir+" >> "+targetDir);
-		wrench.copyDirSyncRecursive(sourceDir,targetDir,{
-			forceDelete: true
-		},function(){
-			grunt.log.writeln(sourceDir+" >> "+targetDir);
+
+		function run_salt_prep(){
+
 			grunt.log.writeln("run salt env base");
 			ls    = spawn('sh', ['/srv/salt/boot/bootstrap-salt.sh','-K','stable']);
 			var lastout;
@@ -42,10 +55,7 @@ module.exports = function(grunt) {
 			ls.on('exit', function (code) {
 			  console.log('child process exited with code ' + code);
 			});
-
 			
-
-
 			var sourceDir = 'server/salt/deploy_minions/';
 			var targetDir = '/etc/salt/minion.d/';
 			wrench.copyDirSyncRecursive(sourceDir,targetDir,{
@@ -63,39 +73,25 @@ module.exports = function(grunt) {
 						env_obj.push(app_key);
 					}
 				}
-
-				function run_env(env_obj){
-					var current_env = env_obj[0];
-					env_obj.shift();
-					ls    = spawn('salt-call', ['--local','--log-level=info','--config-dir=/etc/salt','state.highstate','env='+current_env]);
-					var lastout;
-					ls.stdout.on('data', function (data) {
-						var out = data.toString().trim();
-						if( out!='\n' && out!=null && out!="" && lastout!=out){
-							lastout=out;
-							console.log(out);
-						}
-					});
-					ls.stderr.on('data', function (data) {
-					  console.log('stderr: ' + data);
-					});
-					ls.on('exit', function (code) {
-						console.log('child process exited with code ' + code);
-						grunt.log.writeln("<<<<<<<< finished sever "+current_env);
-						
-						if(env_obj.length>0){
-							run_env(env_obj);
-						}
-					});
-				}
 				run_env(env_obj);
-
-
-				
 			});
+		}
 
-		
-		});
+
+
+		if (!fs.existsSync("/srv/salt")) {
+			fs.mkdir("/srv/salt", 0777, true, function (err) {
+				if (err) {
+					grunt.log.writeln("failed to make folder server");
+				} else {
+					grunt.log.writeln("made folder server");
+				}
+			});
+		}
+		var sourceDir = 'tasks/jigs/salt';
+		var targetDir = '/srv/salt';
+		grunt.log.writeln("about to move "+sourceDir+" >> "+targetDir);
+		wrench.copyDirSyncRecursive(sourceDir,targetDir,{ forceDelete: true },run_salt_prep);
 
 		
 		
