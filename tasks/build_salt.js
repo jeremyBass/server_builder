ab15226adaaf740fe546bc2581ffc1b924bac4ff
Jeremy Bass <jeremybass@cableone.net>
2014-11-27 13:52:28 -0800

    remove needless function

--- tasks/build_salt.js
+++ tasks/build_salt.js
@@ -4,15 +4,7 @@
 */
 module.exports = function(grunt) {
 	grunt.registerTask('build_salt', 'Setting up the salt provisioner', function() {
-		function cmd_exec(cmd, args, cb_stdout, cb_end) {
-			var spawn = require('child_process').spawn,
-				child = spawn(cmd, args),
-				me = this;
-			me.exit = 0;  // Send a cb to set 1 when cmd exits
-			child.stdout.on('data', function (data) { cb_stdout(me, data) });
-			child.stdout.on('end', function () { cb_end(me) });
-		}
-		
+
 		var nunjucks = require('nunjucks');
 		var fs = require('fs');
 		var extend = require('extend');
