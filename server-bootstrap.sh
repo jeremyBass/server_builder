4744fc4a8add980779a3d00d4175e0f6cda5c31c
Jeremy Bass <jeremybass@cableone.net>
2014-12-21 15:09:54 -0800

    alter the server-bootstrap to check for things and not do if not needed

--- server-bootstrap.sh
+++ server-bootstrap.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 echo "Server type is: ${SERVER_TYPE}"
 mkdir -p /srv/builder
-if [ $SERVER_TYPE = "VAGRANT" ]; then
-	cp /vagrant/server_project.conf /srv/builder/server_project.conf
-fi
-cd /
-curl  https://raw.githubusercontent.com/jeremyBass/gitploy/master/gitploy | sudo sh -s -- install
-[ -h /usr/sbin/gitploy ] || echoerr "gitploy failed install"
 
-yum install -y epel-release
-yum install -y nodejs
-yum install -y npm
-npm install -g grunt-cli
+if [ $SERVER_TYPE = "VAGRANT" ]; then
+    cp /vagrant/server_project.conf /srv/builder/server_project.conf
+fi
+
+cd /
+if ! -h /usr/sbin/gitploy; then
+    curl  https://raw.githubusercontent.com/jeremyBass/gitploy/master/gitploy | sudo sh -s -- install
+    [ -h /usr/sbin/gitploy ] || echoerr "gitploy failed install"
+fi
+
+rpm -qa | grep -qw epel-release || yum install -y epel-release
+rpm -qa | grep -qw nodejs || yum install -y nodejs
+rpm -qa | grep -qw npm || yum install -y npm
+
+if ! rpm -qa | grep -qw glibc-npm; then
+    yum install -y npm
+    npm install -g grunt-cli
+fi
 
 [ -d /srv/builder ] || mkdir -p /srv/builder
 
 gitploy init 2>&1 | grep -qi "already initialized" && echo ""
 gitploy ls 2>&1 | grep -qi "serverbase" && gitploy up serverbase
 gitploy ls 2>&1 | grep -qi "serverbase" || gitploy add -p /srv/builder serverbase https://github.com/jeremyBass/server_builder.git
-
-
 
 cd /srv/builder
 npm install
