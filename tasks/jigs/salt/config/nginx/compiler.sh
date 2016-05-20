5c51762a6cf3362aa3287b7e4532c9dca080b051
Jeremy Bass <jeremybass@cableone.net>
2016-05-20 09:49:25 -0700

    use the scl to get gcc 4.8

--- tasks/jigs/salt/config/nginx/compiler.sh
+++ tasks/jigs/salt/config/nginx/compiler.sh
@@ -23,6 +23,7 @@ yum -y install gcc-c++ pcre-devel zlib-devel make unzip
 rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
 wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
 yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
+scl enable devtoolset-2 bash
 PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
 {% endif -%}
 
