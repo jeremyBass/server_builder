a1ce68cb2e311449fe30774e8ed690ff380b6797
Jeremy Bass <jeremybass@cableone.net>
2016-05-21 03:02:06 -0700

    change how MS is build per the docs... also reorder the nginx build for mods first to fail faster when needed

--- tasks/jigs/salt/config/nginx/compiler.sh
+++ tasks/jigs/salt/config/nginx/compiler.sh
@@ -17,14 +17,6 @@ msVersion="{{ nginx['msVersion'] }}"
 touch /var/log/failed_nginx_compile.log
 touch /var/log/nginx-${nginxVersion}_compile.log
 
-{% if nginx['npsVersion']  != "false" -%}
-#for pagespeed
-yum -y install gcc-c++ pcre-devel zlib-devel make unzip
-rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
-wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
-yum -y install devtoolset-2-binutils devtoolset-2-gcc-c++
-PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
-{% endif -%}
 
 #set the compiler to be quite
 #then return message only it it's a fail
@@ -49,11 +41,12 @@ ini(){
 
 {% if nginx['msVersion'] != "false" %}
     cd /src/nginx/
+    yum -y install httpd httpd-devel pcre pcre-devel libxml2-devel
     # Fetch modsecurity
     wget -N -O modsecurity-${msVersion}.tar.gz https://github.com/SpiderLabs/ModSecurity/releases/download/v${msVersion}/modsecurity-${msVersion}.tar.gz 2>/var/log/nginx-${nginxVersion}_compile.log
     tar -xzf modsecurity-${msVersion}.tar.gz
     cd modsecurity-${msVersion}
-    ./configure --enable-standalone-module
+    ./configure --enable-standalone-module --disable-mlogc
     make && make install  2>/var/log/nginx-${nginxVersion}_compile.log
 {% endif %}
 
@@ -66,6 +59,13 @@ ini(){
 {% endif -%}
 
 {% if nginx['npsVersion']  != "false" -%}
+    #for pagespeed
+    yum -y install gcc-c++ pcre-devel zlib-devel make unzip
+    rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
+    wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
+    yum -y install devtoolset-2-binutils devtoolset-2-gcc-c++
+    PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
+
     #get page speed
     wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${npsVersion}-beta.zip 2>/var/log/nginx-${nginxVersion}_compile.log
     unzip release-${npsVersion}-beta 2>/var/log/nginx-${nginxVersion}_compile.log
@@ -86,26 +86,6 @@ ini(){
 --group={{ nginx['user'] }} \
 --prefix=/etc/nginx \
 --sbin-path=/usr/sbin/nginx \
---conf-path=/etc/nginx/nginx.conf \
---pid-path=/var/run/nginx.pid \
---lock-path=/var/lock/subsys/nginx \
---error-log-path=/var/log/nginx/error.log \
---http-log-path=/var/log/nginx/access.log \
---http-client-body-temp-path=/var/cache/nginx/client_temp \
---http-proxy-temp-path=/var/cache/nginx/proxy_temp \
---http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
---http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
---http-scgi-temp-path=/var/cache/nginx/scgi_temp \
---with-http_auth_request_module \
---with-http_sub_module \
---with-http_mp4_module \
---with-http_flv_module \
---with-http_addition_module \
---with-http_dav_module \
---with-http_gunzip_module \
---with-http_gzip_static_module \
---with-http_stub_status_module \
---with-http_sub_module \
 {% if nginx['msVersion']  != "false" -%}
 --add-module=/src/nginx/modsecurity-${msVersion}/nginx/modsecurity \
 {% endif -%}
@@ -116,15 +96,35 @@ ini(){
 --with-sha1=/usr/include/openssl \
 --with-md5=/usr/include/openssl \
 {% endif -%}
+{% if nginx['npsVersion']  != "false" -%}
+--add-module=/src/nginx/ngx_pagespeed-release-${npsVersion}-beta ${PS_NGX_EXTRA_FLAGS} \
+{% endif -%}
+--conf-path=/etc/nginx/nginx.conf \
+--pid-path=/var/run/nginx.pid \
+--lock-path=/var/lock/subsys/nginx \
+--error-log-path=/var/log/nginx/error.log \
+--http-log-path=/var/log/nginx/access.log \
+--http-client-body-temp-path=/var/cache/nginx/client_temp \
+--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
 --with-pcre \
 --with-ipv6 \
 --with-file-aio \
 --with-http_realip_module \
 --without-http_scgi_module \
 --without-http_uwsgi_module \
-{% if nginx['npsVersion']  != "false" -%}
---add-module=/src/nginx/ngx_pagespeed-release-${npsVersion}-beta ${PS_NGX_EXTRA_FLAGS}
-{% endif -%}
+--with-http_auth_request_module \
+--with-http_sub_module \
+--with-http_mp4_module \
+--with-http_flv_module \
+--with-http_addition_module \
+--with-http_dav_module \
+--with-http_gunzip_module \
+--with-http_gzip_static_module \
+--with-http_stub_status_module \
+--with-http_sub_module
     make && make install  2>/var/log/nginx-${nginxVersion}_compile.log
 }
 
